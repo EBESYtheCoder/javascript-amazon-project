250f4803e612d2d1c4adf755d84a6c035ec0289e
Outlier <nzombeebenezer@gmail.com>
2024-07-26 19:00:35 +0100

    fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -27,7 +28,7 @@ cart.forEach((cartItem) => {
               ${matchingProduct.name}
             </div>
             <div class="product-price">
-              ${matchingProduct.priceCents / 100}
+              ${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
               <span>
@@ -47,7 +48,7 @@ cart.forEach((cartItem) => {
               Choose a delivery option:
             </div>
             <div class="delivery-option">
-              <input type="radio" checked class="delivery-option-input" name="delivery-option-1">
+              <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
               <div>
                 <div class="delivery-option-date">
                   Tuesday, June 21
@@ -58,7 +59,7 @@ cart.forEach((cartItem) => {
               </div>
             </div>
             <div class="delivery-option">
-              <input type="radio" class="delivery-option-input" name="delivery-option-1">
+              <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
               <div>
                 <div class="delivery-option-date">
                   Wednesday, June 15
@@ -69,7 +70,7 @@ cart.forEach((cartItem) => {
               </div>
             </div>
             <div class="delivery-option">
-              <input type="radio" class="delivery-option-input" name="delivery-option-1">
+              <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
               <div>
                 <div class="delivery-option-date">
                   Monday, June 13
