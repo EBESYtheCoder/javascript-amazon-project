f4851589df0eb676326cd377daa8e12711a5f9b1
Outlier <nzombeebenezer@gmail.com>
2024-07-27 19:26:12 +0100

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D,'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -107,9 +104,10 @@ export function renderOrderSummary() {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-
       const container = document.querySelector(`.js-cart-item-container-${productId}`)
       container.remove();
+
+      renderPaymentSummary();
     })
   })
 
@@ -118,6 +116,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     })
   })
 };
