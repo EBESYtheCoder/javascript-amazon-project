a950be66aa1c8274846d0096ac1a6f93694895c4
Outlier <nzombeebenezer@gmail.com>
2024-07-30 00:01:15 +0100

    create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStrorage();
 
-if (!cart) {
-  cart = [{
+export function loadFromStrorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
+  if (!cart) {
+    cart = [{
 
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 
 function saveToStorage() {
